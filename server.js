41ccc4a9dd559108454972467fd0f3d28f68ce21
Jessy Castaneda <jessyc.12@icloud.com>
2024-06-12 12:29:52 -0700

    pushing original code" .
    ,

--- server.js
+++ server.js
@@ -1,54 +1,44 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
 const session = require('express-session');
-const routes = require('./controllers');
-const env = require('dotenv').config;
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('Database connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+const sequelize = require('./config/config');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({    
-    db: sequelize 
-  })
-};
-
-app.use(session(sess));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
-
-module.exports = sequelize;
+    secret: process.env.SAM,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  
+  app.use(session(sess));
+  
+  const hbs = exphbs.create({
+    helpers: {
+      format_date: date => {
+        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+      }
+    }
+  });
+  
+  app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+  app.use(require('./controllers/'));
+  
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+    sequelize.sync({ force: false });
+  });
